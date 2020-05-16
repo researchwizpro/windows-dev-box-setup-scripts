d03ea42fb6fcf32f9fc3443cc1ab377bc168249f
researchwizpro <65458634+researchwizpro@users.noreply.github.com>
2020-05-16 15:37:42 -0700

    Update dev_app.ps1

--- dev_app.ps1
+++ dev_app.ps1
@@ -22,9 +22,12 @@ function executeScript {
 }
 
 #--- Setting up Windows ---
+executeScript "ChangeExecutionPolicy.ps1";
+executeScript "RemoveDefaultApps.ps1";
+executeScript "InstallApps.ps1";
 executeScript "SystemConfiguration.ps1";
 executeScript "FileExplorerSettings.ps1";
-#executeScript "RemoveDefaultApps.ps1";
+
 #executeScript "CommonDevTools.ps1";
 
 
